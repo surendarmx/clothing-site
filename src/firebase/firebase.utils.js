da0b66f3e06ba9319444d8f8ca52b1a33995edd3
Surendar <surendarmx@gmail.com>
2021-01-04 00:50:16 +0530

    implemnted firebase utils, includes ability t authenticate user into our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     measurementId: "G-CKNRSY3388"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+
+    
+    if(!snapshot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        }catch (error) {
+            console.log('rror creating user', error.message);
+        }
+    }
+    return userRef;
+  }
+
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
@@ -20,5 +46,10 @@ const config = {
   const provider = new firebase.auth.GoogleAuthProvider();
   provider.setCustomParameters({prompt: 'select_account'});
   export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  //firestore.collection('users').doc('MBeBQmuzzN67NLn2bAGV').collection('cartItems').doc('ugw9ItJxXs1BB3m9G5Zj')
+  //firestore.doc('/users/MBeBQmuzzN67NLn2bAGV/cartItems/ugw9ItJxXs1BB3m9G5Zj')
+  //firestore.collection('/users/MBeBQmuzzN67NLn2bAGV/cartItems')
+
 
   export default firebase;
