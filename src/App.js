5bd24c4e0bd5b40c4c4053c772840d8fae5dd985
Surendar <surendarmx@gmail.com>
2021-01-04 23:56:22 +0530

    added rdux and implemented user reducer and action to header and app components

--- src/App.js
+++ src/App.js
@@ -4,33 +4,30 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { connect } from 'react-redux';
 import './App.css';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-constructor() {
-  super();
-
-  this.state = {
-    currentUser: null
-  }
-}
 
 unsubscribeFromAuth = null;
 
 componentDidMount() {
+
+const {setCurrentUser} = this.props;
+
   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if(userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
       userRef.onSnapshot(snapshot => {
-        this.setState({ currentUser: {
+        setCurrentUser({ currentUser: {
           id: snapshot.id,
           ...snapshot.data()
         }});
       })
     }
-    this.setState({currentUser: userAuth})
+    setCurrentUser({currentUser: userAuth})
   });
 }
 
@@ -41,7 +38,7 @@ componentWillUnmount() {
   render() {
     return (
       <div>
-      <Header currentUser={this.state.currentUser}/>
+      <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -53,4 +50,8 @@ componentWillUnmount() {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
